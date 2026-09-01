1662bc489eeb63a9fa3fa3a335cc1126d1a5037e
Victor Alves <victor.rocha.alves396@gmail.com>
2026-09-01 20:54:09 -0300

    feat: Criação dos arquivos Corpo e Universo.

--- SimuladorGravitacional/Form1.Designer.cs
+++ SimuladorGravitacional/Form1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1475, 506);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ SimuladorGravitacional/Universo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimuladorGravitacional
+{
+    internal class Universo
+    {
+    }
+}
