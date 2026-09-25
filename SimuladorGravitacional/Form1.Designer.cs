b40f7cb5c3cd418203a2c1c55544f4939386dbae
EliezerNovai <ryderking090@gmail.com>
2026-09-25 15:36:57 -0300

    feat: Desenhar os corpos no panel.

--- SimuladorGravitacional/Form1.Designer.cs
+++ SimuladorGravitacional/Form1.Designer.cs
@@ -59,30 +59,28 @@
             // 
             // panel1
             // 
-            panel1.BackColor = SystemColors.ControlDark;
-            panel1.Location = new Point(11, 93);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.BackColor = SystemColors.Desktop;
+            panel1.Location = new Point(10, 70);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1237, 700);
+            panel1.Size = new Size(1082, 525);
             panel1.TabIndex = 2;
             panel1.Paint += panel1_Paint_1;
             // 
             // button1
             // 
-            button1.Location = new Point(11, 16);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(10, 12);
             button1.Name = "button1";
-            button1.Size = new Size(207, 31);
+            button1.Size = new Size(181, 23);
             button1.TabIndex = 3;
             button1.Text = "Iniciar uma nova configuração";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
             // 
             // button2
             // 
-            button2.Location = new Point(1040, 16);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(910, 12);
             button2.Name = "button2";
-            button2.Size = new Size(207, 31);
+            button2.Size = new Size(181, 23);
             button2.TabIndex = 4;
             button2.Text = "Carregar uma configuração";
             button2.UseVisualStyleBackColor = true;
@@ -90,10 +88,9 @@
             // 
             // button3
             // 
-            button3.Location = new Point(1040, 55);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Location = new Point(910, 41);
             button3.Name = "button3";
-            button3.Size = new Size(207, 31);
+            button3.Size = new Size(181, 23);
             button3.TabIndex = 5;
             button3.Text = "Salvar configuração atual";
             button3.UseVisualStyleBackColor = true;
@@ -104,7 +101,7 @@
             Massa.AutoSize = true;
             Massa.Location = new Point(3, 0);
             Massa.Name = "Massa";
-            Massa.Size = new Size(50, 20);
+            Massa.Size = new Size(40, 15);
             Massa.TabIndex = 8;
             Massa.Text = "Massa";
             // 
@@ -113,7 +110,7 @@
             Densidade.AutoSize = true;
             Densidade.Location = new Point(3, 0);
             Densidade.Name = "Densidade";
-            Densidade.Size = new Size(72, 40);
+            Densidade.Size = new Size(62, 15);
             Densidade.TabIndex = 11;
             Densidade.Text = "Densidade";
             // 
@@ -122,86 +119,93 @@
             flowLayoutPanel1.Controls.Add(flowLayoutPanel3);
             flowLayoutPanel1.Controls.Add(numericUpDown1);
             flowLayoutPanel1.Controls.Add(numericUpDown2);
-            flowLayoutPanel1.Location = new Point(701, 12);
+            flowLayoutPanel1.Location = new Point(613, 9);
+            flowLayoutPanel1.Margin = new Padding(3, 2, 3, 2);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(274, 35);
+            flowLayoutPanel1.Size = new Size(240, 26);
             flowLayoutPanel1.TabIndex = 12;
             // 
             // flowLayoutPanel3
             // 
             flowLayoutPanel3.Controls.Add(Massa);
-            flowLayoutPanel3.Location = new Point(3, 4);
-            flowLayoutPanel3.Margin = new Padding(3, 4, 3, 4);
+            flowLayoutPanel3.Location = new Point(3, 3);
             flowLayoutPanel3.Name = "flowLayoutPanel3";
-            flowLayoutPanel3.Size = new Size(80, 27);
+            flowLayoutPanel3.Size = new Size(70, 20);
             flowLayoutPanel3.TabIndex = 0;
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(89, 4);
-            numericUpDown1.Margin = new Padding(3, 4, 3, 4);
+            numericUpDown1.Location = new Point(79, 3);
+            numericUpDown1.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 200, 0, 0, 0 });
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(85, 27);
+            numericUpDown1.Size = new Size(74, 23);
             numericUpDown1.TabIndex = 9;
+            numericUpDown1.Value = new decimal(new int[] { 500, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
             // 
             // numericUpDown2
             // 
-            numericUpDown2.Location = new Point(180, 4);
-            numericUpDown2.Margin = new Padding(3, 4, 3, 4);
-            numericUpDown2.Maximum = new decimal(new int[] { 800, 0, 0, 0 });
+            numericUpDown2.Location = new Point(159, 3);
+            numericUpDown2.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numericUpDown2.Minimum = new decimal(new int[] { 501, 0, 0, 0 });
             numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new Size(85, 27);
+            numericUpDown2.Size = new Size(74, 23);
             numericUpDown2.TabIndex = 10;
-            numericUpDown2.Value = new decimal(new int[] { 800, 0, 0, 0 });
+            numericUpDown2.Value = new decimal(new int[] { 1000, 0, 0, 0 });
+            numericUpDown2.ValueChanged += numericUpDown2_ValueChanged;
             // 
             // flowLayoutPanel2
             // 
             flowLayoutPanel2.Controls.Add(flowLayoutPanel4);
             flowLayoutPanel2.Controls.Add(numericUpDown3);
             flowLayoutPanel2.Controls.Add(numericUpDown4);
-            flowLayoutPanel2.Location = new Point(701, 53);
+            flowLayoutPanel2.Location = new Point(613, 40);
+            flowLayoutPanel2.Margin = new Padding(3, 2, 3, 2);
             flowLayoutPanel2.Name = "flowLayoutPanel2";
-            flowLayoutPanel2.Size = new Size(274, 35);
+            flowLayoutPanel2.Size = new Size(240, 26);
             flowLayoutPanel2.TabIndex = 13;
             // 
             // flowLayoutPanel4
             // 
             flowLayoutPanel4.Controls.Add(Densidade);
-            flowLayoutPanel4.Location = new Point(3, 4);
-            flowLayoutPanel4.Margin = new Padding(3, 4, 3, 4);
+            flowLayoutPanel4.Location = new Point(3, 3);
             flowLayoutPanel4.Name = "flowLayoutPanel4";
-            flowLayoutPanel4.Size = new Size(80, 27);
+            flowLayoutPanel4.Size = new Size(70, 20);
             flowLayoutPanel4.TabIndex = 9;
             // 
             // numericUpDown3
             // 
             numericUpDown3.DecimalPlaces = 2;
-            numericUpDown3.Location = new Point(89, 4);
-            numericUpDown3.Margin = new Padding(3, 4, 3, 4);
+            numericUpDown3.Location = new Point(79, 3);
+            numericUpDown3.Maximum = new decimal(new int[] { 2259, 0, 0, 131072 });
+            numericUpDown3.Minimum = new decimal(new int[] { 9, 0, 0, 131072 });
             numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new Size(85, 27);
+            numericUpDown3.Size = new Size(74, 23);
             numericUpDown3.TabIndex = 11;
             numericUpDown3.Value = new decimal(new int[] { 9, 0, 0, 131072 });
+            numericUpDown3.ValueChanged += numericUpDown3_ValueChanged;
             // 
             // numericUpDown4
             // 
             numericUpDown4.DecimalPlaces = 2;
-            numericUpDown4.Location = new Point(180, 4);
-            numericUpDown4.Margin = new Padding(3, 4, 3, 4);
+            numericUpDown4.Location = new Point(159, 3);
             numericUpDown4.Maximum = new decimal(new int[] { 2260, 0, 0, 131072 });
-            numericUpDown4.Minimum = new decimal(new int[] { 9, 0, 0, 131072 });
+            numericUpDown4.Minimum = new decimal(new int[] { 10, 0, 0, 131072 });
             numericUpDown4.Name = "numericUpDown4";
-            numericUpDown4.Size = new Size(85, 27);
+            numericUpDown4.Size = new Size(74, 23);
             numericUpDown4.TabIndex = 12;
             numericUpDown4.Value = new decimal(new int[] { 2260, 0, 0, 131072 });
+            numericUpDown4.ValueChanged += numericUpDown4_ValueChanged;
             // 
             // flowLayoutPanel5
             // 
             flowLayoutPanel5.Controls.Add(label1);
             flowLayoutPanel5.Controls.Add(numericUpDown5);
-            flowLayoutPanel5.Location = new Point(498, 27);
+            flowLayoutPanel5.Location = new Point(436, 20);
+            flowLayoutPanel5.Margin = new Padding(3, 2, 3, 2);
             flowLayoutPanel5.Name = "flowLayoutPanel5";
-            flowLayoutPanel5.Size = new Size(187, 41);
+            flowLayoutPanel5.Size = new Size(164, 31);
             flowLayoutPanel5.TabIndex = 14;
             // 
             // label1
@@ -209,23 +213,25 @@
             label1.AutoSize = true;
             label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(56, 20);
+            label1.Size = new Size(45, 15);
             label1.TabIndex = 0;
             label1.Text = "Corpos";
             // 
             // numericUpDown5
             // 
-            numericUpDown5.Location = new Point(65, 3);
+            numericUpDown5.Location = new Point(54, 2);
+            numericUpDown5.Margin = new Padding(3, 2, 3, 2);
+            numericUpDown5.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
             numericUpDown5.Name = "numericUpDown5";
-            numericUpDown5.Size = new Size(102, 27);
+            numericUpDown5.Size = new Size(89, 23);
             numericUpDown5.TabIndex = 1;
             numericUpDown5.ValueChanged += numericUpDown5_ValueChanged;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1259, 803);
+            ClientSize = new Size(1102, 602);
             Controls.Add(flowLayoutPanel5);
             Controls.Add(flowLayoutPanel2);
             Controls.Add(flowLayoutPanel1);
@@ -233,6 +239,7 @@
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
--- SimuladorGravitacional/Form1.cs
+++ SimuladorGravitacional/Form1.cs
@@ -1,21 +1,19 @@
+using System.ComponentModel;
+
 namespace SimuladorGravitacional
 {
     public partial class Form1 : Form
     {
-
+        Universo universo = new Universo(0, 0, 0, 0, 0, 0, 0);
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int height = panel1.Height;
-            int width = panel1.Width;
-            double minMassa = (double)numericUpDown1.Value;
-            double maxMassa = (double)numericUpDown2.Value;
-            double minDensidade = (double)numericUpDown3.Value;
-            double maxDensidade = (double)numericUpDown4.Value;
+
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -45,12 +43,58 @@ namespace SimuladorGravitacional
 
         private void panel1_Paint_1(object sender, PaintEventArgs e)
         {
+            foreach (Corpo corpo in universo.corpos)
+            {
+                e.Graphics.FillEllipse(Brushes.White,
+                    (float)corpo.posX - (float)corpo.Raio(),
+                    (float)corpo.posY - (float)corpo.Raio(),
+                    (float)corpo.Raio() * 2,
+                    (float)corpo.Raio() * 2);
+            }
 
         }
 
         private void numericUpDown5_ValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            universo = new Universo((int)numericUpDown5.Value, panel1.Height, panel1.Width, (double)numericUpDown1.Value, (double)numericUpDown2.Value, (double)numericUpDown3.Value, (double)numericUpDown4.Value);
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value >= numericUpDown2.Value)
+            {
+                numericUpDown2.Value = numericUpDown1.Value + 1;
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Value <= numericUpDown1.Value)
+            {
+                numericUpDown1.Value = numericUpDown2.Value - 1;
+            }
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown3.Value >= numericUpDown4.Value)
+            {
+                numericUpDown4.Value = numericUpDown3.Value + (decimal)0.01;
+            }
+        }
+        
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown4.Value <= numericUpDown3.Value)
+            {
+                numericUpDown3.Value = numericUpDown4.Value - (decimal)0.01;
+            }
         }
     }
 }
